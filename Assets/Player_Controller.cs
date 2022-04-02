9f3214370211d266d1ea6635d984d0716aa94af7
DrVital <sokolov11061@yandex.ru>
2022-04-02 19:53:39 +0300

    Mechanics_Update
    Добавили механики

--- Assets/Player_Controller.cs
+++ Assets/Player_Controller.cs
@@ -15,30 +15,78 @@ public class Player_Controller : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        walk();
-        jump();
+        Walk();
+        Jump();
+        SlowWalk();
+        InteractWithSmth();
     }
 
-    public float speed = 5, Jump_Power = 500;
+    public float speed = 5, Jump_Power = 500, slow_speed = 2;
     private bool onGround = true;
 
+
+
     public LayerMask layerOnGround;
+    public LayerMask interactiveLayer;
 
-    private float GroundConnection = 0.8f;
+    private float groundconnection = 0.8f;
+    //private float interactSphere_radius = transform.localScale.x;
+   // private float interactSphere_radius = 1.5f;
+   // private GameObject 
 
-    private void walk() 
+
+    private void Walk() 
     {
-        Player_Body.velocity = new Vector3(Input.GetAxis("Horizontal") * speed, Player_Body.velocity.y, Player_Body.velocity.z);
+        //     Player_Body.velocity = new Vector3(Input.GetAxis("Horizontal") * speed, Player_Body.velocity.y, Player_Body.velocity.z);
+        Player_Body.velocity = new Vector3(Input.GetAxis("Horizontal") * speed, Player_Body.velocity.y, Input.GetAxis("Vertical") * speed);
     }
 
-  
+    private void SlowWalk()
+    {
+        if(Input.GetKey(KeyCode.LeftShift))
+            Player_Body.velocity = new Vector3(Input.GetAxis("Horizontal") * slow_speed, Player_Body.velocity.y, Input.GetAxis("Vertical") * slow_speed);
+    }
 
-    private void jump()
+    private void Jump()
     {
         //  if (Physics.OverlapSphere(GroundCheckSphere.position, 0.5f)[0].na);
-        onGround = Physics.Raycast(transform.position, Vector3.down, GroundConnection, layerOnGround.value);
+        onGround = Physics.Raycast(transform.position, Vector3.down, groundconnection, layerOnGround.value);
         if (Input.GetKeyDown(KeyCode.Space) && onGround)
             Player_Body.AddForce(new Vector3(0,Jump_Power,0));
     }
 
+    /*   private void interactSphere()
+       {
+          if (Physics.OverlapSphere(transform.position, interactSphere_radius, interactiveLayer) != null)
+           Debug.Log(Physics.OverlapSphere(transform.position, interactSphere_radius, interactiveLayer)[0].name);
+
+       }
+    */
+
+    private GameObject interactAim = null;
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("interactive"))
+        {
+            interactAim = other.gameObject;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject == interactAim)
+            interactAim = null;
+    }
+
+    private void InteractWithSmth() //Вставить в update
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (interactAim != null)
+            {
+                Debug.Log(interactAim.name);
+            }
+        }
+
+    }
 }
